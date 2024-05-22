f8a274a5ddbfd8c04cf5174ead23fd02977e9fec
Alex Varas <346508+alej0varas@users.noreply.github.com>
2024-05-22 21:32:07 +0200

    Add argparse for command line arguments

--- bcp/main.py
+++ bcp/main.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import threading
 import time
@@ -281,6 +282,21 @@ def main(url, fullscreen=True, skip_downloaded=False):
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    # TODO: get from --skip_downloaded --fullscreen
-    main(url, fullscreen=True, skip_downloaded=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="Band, album or track url.")
+    parser.add_argument(
+        "-s",
+        "--skip-downloaded",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Skip songs that have already been downloaded.",
+    )
+    parser.add_argument(
+        "-f",
+        "--fullscreen",
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="Use fullscreen.",
+    )
+    args = parser.parse_args()
+    main(args.url, fullscreen=args.fullscreen, skip_downloaded=args.skip_downloaded)
