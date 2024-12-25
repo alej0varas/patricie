ab2a535f7fe963578d0f013a765f1d6eaa20b0fa
Alex Varas <346508+alej0varas@users.noreply.github.com>
2024-12-25 14:23:47 +0100

    Improve logger
    - Open log file in user data dir
    - Always log not only when in debug

--- src/bcp/log.py
+++ src/bcp/log.py
@@ -9,16 +9,18 @@ DEBUG = os.environ.get("DEBUG", False)
 
 class Log:
     def __call__(self, *args):
-        if DEBUG:
-            with open(".log.txt", "a") as f:
-                match args:
-                    case int():
-                        a = str(args)
-                    case list() | tuple():
-                        a = "".join(map(str, args))
-                    case _:
-                        a = args
-                f.write(f"{self.caller} {a}\n")
+        from . import utils
+
+        path = os.path.join(utils.USER_DATA_DIR, ".log.txt")
+        with open(path, "a") as f:
+            match args:
+                case int():
+                    a = str(args)
+                case list() | tuple():
+                    a = "".join(map(str, args))
+                case _:
+                    a = args
+            f.write(f"{self.caller} {a}\n")
 
     def __init__(self, caller):
         self.caller = caller
